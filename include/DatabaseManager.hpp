cfb03bc4a61eebc4a3500c6f3995bce851b070ab
Mahdi Movahedi <m.movahedimonfared@gmail.com>
2025-04-20 21:17:54 +0330

    running clang-format
    Signed-off-by: Mahdi Movahedi <m.movahedimonfared@gmail.com>

--- include/DatabaseManager.hpp
+++ include/DatabaseManager.hpp
@@ -10,52 +10,51 @@ namespace PerfMgmt {
 
 class DatabaseManager {
 public:
-  DatabaseManager(DatabaseManager &other) = delete;
-  DatabaseManager(DatabaseManager &&other) = delete;
-  explicit DatabaseManager(const std::string &dbAddress);
-  ~DatabaseManager() = default;
+    DatabaseManager(DatabaseManager& other) = delete;
+    DatabaseManager(DatabaseManager&& other) = delete;
+    explicit DatabaseManager(const std::string& dbAddress);
+    ~DatabaseManager() = default;
 
-  bool InitializeDatabase();
+    bool InitializeDatabase();
 
-  // ---- Employee management ----
+    // ---- Employee management ----
 
-  // 1. addEmployee
-  bool addEmployee(const Employee &employee);
-  // 2. getEmployee
-  Employee getEmployee(int emplyeeId);
-  // 3. getAllEmployees
-  std::optional<std::vector<Employee>> getAllEmployees();
-  // 4. getDirectReports
-  std::optional<performanceReview> getEmployeesReportingToHead(int reviewerId);
+    // 1. addEmployee
+    bool addEmployee(const Employee& employee);
+    // 2. getEmployee
+    Employee getEmployee(int emplyeeId);
+    // 3. getAllEmployees
+    std::optional<std::vector<Employee>> getAllEmployees();
+    // 4. getDirectReports
+    std::optional<performanceReview> getEmployeesReportingToHead(int reviewerId);
 
-  // 5. updateEmployee
-  bool updateEmployee(const Employee &employee);
+    // 5. updateEmployee
+    bool updateEmployee(const Employee& employee);
 
-  // 6. deactivateEmployee
-  bool deactivateEmployee(int employeeId);
+    // 6. deactivateEmployee
+    bool deactivateEmployee(int employeeId);
 
-  // ---- Performance Review Management ----
+    // ---- Performance Review Management ----
 
-  // 1. addPerformanceReivew
-  bool updatePerformanceReviwe(const performanceReview &review);
-  // 2. getPerformanceReview by id
-  std::optional<performanceReview> getPerformanceReview(int reviewId);
-  // 3. getReviewForEmployee
-  std::optional<performanceReview> getPerformanceForEmployee(int employeeId);
-  // 4. getReviewByReviewer
-  std::optional<std::vector<performanceReview>>
-  getReviewByReviewe(int reviewerId);
-  // 5. updatePerformanceReview
-  bool updateperformanceReview(int reviewId);
-  // 6. deletePerformanceReivew
-  bool deletePerformanceReview(int reviewId);
+    // 1. addPerformanceReivew
+    bool updatePerformanceReviwe(const performanceReview& review);
+    // 2. getPerformanceReview by id
+    std::optional<performanceReview> getPerformanceReview(int reviewId);
+    // 3. getReviewForEmployee
+    std::optional<performanceReview> getPerformanceForEmployee(int employeeId);
+    // 4. getReviewByReviewer
+    std::optional<std::vector<performanceReview>> getReviewByReviewe(int reviewerId);
+    // 5. updatePerformanceReview
+    bool updateperformanceReview(int reviewId);
+    // 6. deletePerformanceReivew
+    bool deletePerformanceReview(int reviewId);
 
 private:
-  sqlite::database db;
+    sqlite::database db;
 
-  // map database entry to Employee datastructure
+    // map database entry to Employee datastructure
 
-  // map performance review to PerformanceReview data structure
+    // map performance review to PerformanceReview data structure
 
 protected:
 };
--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,18 @@
 #include <stdexcept>
 #include <string>
 
-int main()
-{
+int main() {
     PerfMgmt::DatabaseManager db("databaseExample.db");
     PerfMgmt::Employee employee1(0, 20251207, "George Michael", "20200101", PerfMgmt::Role::Boss, true, std::nullopt);
     PerfMgmt::Employee employee2(1, 20251208, "John Nash", "20200101", PerfMgmt::Role::Specialist, true, 1);
-    
 
     db.addEmployee(employee1);
     db.addEmployee(employee2);
     // auto response = db.getEmployee(0);
     auto employees = db.getAllEmployees();
-    std::cout << "---- first employee ----" << std::endl; 
+    std::cout << "---- first employee ----" << std::endl;
     std::cout << employees.value().front() << std::endl;
-    std::cout << "---- second employee ----" << std::endl; 
+    std::cout << "---- second employee ----" << std::endl;
     std::cout << employees.value().back() << std::endl;
     return 0;
 }
